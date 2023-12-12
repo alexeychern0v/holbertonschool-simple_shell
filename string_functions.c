083cc88e0f394dc1cacabc8c9bc408e4206c374f
Ines-Oubabase <inesoubabas56@gmail.com>
2023-12-12 14:26:24 +0100

    added shell.h & string

---
+++ string_functions.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (s != 0)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * _strcmp - performs lexicogarphic comparison of two strings
+ * @s1: the first string
+ * @s2: the second string
+ * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (
